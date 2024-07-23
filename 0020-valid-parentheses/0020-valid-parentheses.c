e9ac91eb69b87d56411933e4d2b90d399d40962c
Sayeda19 <164856388+Sayeda19@users.noreply.github.com>
2024-07-23 20:48:44 +0530

    Time: 3 ms (39.08%), Space: 7.9 MB (14.11%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -0,0 +1,19 @@
+bool isValid(char* s) {
+    int n = strlen(s);
+    char stack[n];
+    int top = -1;
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            stack[++top] = s[i];
+        } else {
+            if (top == -1) return false;
+            if (s[i] == ')' && stack[top] != '(') return false;
+            if (s[i] == '}' && stack[top] != '{') return false;
+            if (s[i] == ']' && stack[top] != '[') return false;
+            top--;
+        }
+    }
+
+    return top == -1;
+}
