01050402367aa2c307cf06d72eb60cf96f6940ab
Sayeda19 <164856388+Sayeda19@users.noreply.github.com>
2024-07-25 10:27:07 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (82.06%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -15,18 +15,14 @@ class Solution {
 
         // Reverse each row
         for (int i = 0; i < n; i++) {
-            reverseRow(matrix[i]);
-        }
-    }
-
-    private void reverseRow(int[] row) {
-        int left = 0, right = row.length - 1;
-        while (left < right) {
-            int temp = row[left];
-            row[left] = row[right];
-            row[right] = temp;
-            left++;
-            right--;
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+                left++;
+                right--;
+            }
         }
     }
 }
